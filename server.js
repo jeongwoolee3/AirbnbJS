ba04039e8e7ee3054d85863966bcdf1d866cc632
JW <jeongwoo_lee3@tmax.co.kr>
2023-04-26 21:03:46 +0900

    feat: header css

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "src", "index.html"));
 });
 
+
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server running...")
 })
