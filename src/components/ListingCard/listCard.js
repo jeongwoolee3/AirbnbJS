b78154ab5b1453819a4bf686e335c3266c77f532
JW <jeongwoo_lee3@tmax.co.kr>
2023-05-02 21:52:02 +0900

    feat : 리스팅 css ..

---
+++ src/components/ListingCard/listCard.js
@@ -0,0 +1,19 @@
+import Component from "../../core/Component.js";
+
+export default class Category extends Component {
+
+    setup() {
+        const item = `
+        <div class="grid-item">
+          <img src="https://picsum.photos/id/1018/500/500" alt="Image 1">
+          <div class="overlay">
+            <div class="overlay-content">
+              <button class="btn">Prev</button>
+              <button class="btn">Next</button>
+            </div>
+          </div>
+        </div>
+        `
+        this.state = { items: new Array(4).fill(item) };
+    }
+}
